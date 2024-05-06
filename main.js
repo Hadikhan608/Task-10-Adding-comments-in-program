5b3895c96ce1d896986a234a620f9b6f6367ce54
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 22:59:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//date: 30-April-2024
+//This program is regarding personal message.
+var myName = "Hadi";
+console.log("Hello dear ".concat(myName, " are you interested in learning typescript?"));
---
+++ main.ts
@@ -0,0 +1,4 @@
+//date: 30-April-2024
+//This program is regarding personal message.
+let myName:string = "Hadi";
+console.log(`Hello dear ${myName} are you interested in learning typescript?`);
